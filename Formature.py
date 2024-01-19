a81444ead5fd7d0e8dee3b838fd84611553fd501
yahya zaizi <146640218+Yahyazaizi@users.noreply.github.com>
2024-01-19 07:41:25 +0100

    Update Formature.py

--- Formature.py
+++ Formature.py
@@ -1,5 +1,6 @@
-from IEmploye import IEmploye
-class Formature(IEmploye):
+import json
+form EmployeFile import Employe
+class Formature(Employe):
     def __init__(self, mtle, nom, dateNaissance, dateEmbuche, salaireBase, heurSup):
         super().__init__(mtle, nom, dateNaissance, dateEmbuche, salaireBase)
         self.heurSup = heurSup
@@ -22,3 +23,41 @@ class Formature(IEmploye):
     def salaireAPayer(self):
         salaireNet = (self.salaireBase + self.heurSup * self.tarifSup ) * (1-self.geIR(self.salaireBase))
         return salaireNet
+   
+    def addFormateurToJSON(self):
+        with open("foramteurs.json","r") as file:
+            data = json.load(file)
+        newForm = {"matricule": self.mtle, 
+                   "nom": self.nom, 
+                   "date naissance": self.dateNaissance, 
+                   "date embuche": self.dateEmbuche,
+                   "salaire base": self.salaireBase,
+                   "heur sup": self.heurSup}
+        data.append(newForm)
+        with open("foramteurs.json","w") as f:
+            json.dump(data,f)
+    def deleteForamteurFromJSON(self,FormteurID):
+        with open("foramteurs.json","r") as file:
+            data = json.load(file)
+        
+        for record in data:
+            if record["matricule"] == FormteurID:
+                del data[data.index(record)]
+        with open("foramteurs.json","w") as f:
+            json.dump(data,f)
+    
+    def findFormateurInJSON(self,FormateurID):
+        with open("foramteurs.json","r") as file:
+            data = json.load(file)
+        
+        for record in data:
+            if record["matricule"] == FormateurID:
+                return record
+    
+    def showAllFarmateur(self):
+        with open("foramteurs.json","r") as file:
+            data = json.load(file)
+        return data
+
+
+        
